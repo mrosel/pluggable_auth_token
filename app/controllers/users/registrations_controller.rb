a207eaaeffe526ee0e0023a4a673140f99ef83cf
Mark Rosel <mark.rosel@currencycloud.com>
2018-05-17 17:22:18 +0100

    override devise create for json

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,4 +3,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def flash(args={})
     puts args.inspect
   end
+  
+  # POST /resource
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      render json: resource, status: :created, location: after_sign_up_path_for(resource)
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      render json: resource.errors.messages, status: :unprocessable_entity
+    end
+  end
 end
