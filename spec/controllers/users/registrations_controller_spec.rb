240f4ab89b125ecb58e879166d95e42447e51757
mac <mac@bbtnetworks.com>
2018-07-24 10:09:29 +0100

    fix pending

--- spec/controllers/users/registrations_controller_spec.rb
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
-describe Users::RegistrationsController do
+describe 'Users::RegistrationsController' do
   let(:new_user) {
-    { user: { email: "email@example.com", password: "new_password"} }
+    { user: { email: 'email@example.com', password: 'new_password'} }
   }
 
-  it "POST /users/registration" do
-    pending("controller test for new user")
+  it 'POST /users/registration' do
+    pending('controller test for new user')
     post '/users/registration', new_user
     response.status.should eql(200)
   end
